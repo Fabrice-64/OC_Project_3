e9be2a7cbe5e08cc7430d03ac4c68f0bdd5bcbb5
Fabrice Jaouen <fab.jaouen@gmail.com>
2019-08-26 10:58:10 +0200

    Adding the game to the repository. It is dealt only with the DocString.

---
+++ maze_game.py
@@ -0,0 +1,27 @@
+"""
+This is a maze game developed in the framework of OpenClassrooms education program as Python Developper
+
+Purpose of the game: the hero must exit the maze. In order to to so, he has to collect 3 items; 
+a needle, 
+a PVC tube, 
+a flask of ether.
+The 3 items are randomly scattered in the maze.
+And then inject the warden the drug
+If he fails, he dies.
+
+
+Classes : 
+Hero,
+Warden,
+Items,
+Walls,
+
+Methods:
+
+Attributes:
+
+This program is based on Pygame.
+"""
+
+import pygame
+pygame.init()
