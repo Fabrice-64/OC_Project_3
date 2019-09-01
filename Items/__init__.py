1dfee3ca539f647557789af7f368178142df0f82
Fabrice-64 <fab.jaouen@gmail.com>
2019-09-01 10:56:29 +0200

    Update the project with a full scale maze

---
+++ Items/__init__.py
@@ -0,0 +1,17 @@
+# Set up classes for OC Project3 : build a maze game
+
+class Items:
+    print("YES")
+
+
+class Warden(Items):
+    pass
+
+class Wall(Items):
+    pass
+
+class Collected(Items):
+    pass
+
+class MacGyver(Items):
+    pass
---
+++ Items/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Game',
+    version='',
+    packages=['Items'],
+    url='',
+    license='',
+    author='fabricejaouen',
+    author_email='',
+    description=''
+)
